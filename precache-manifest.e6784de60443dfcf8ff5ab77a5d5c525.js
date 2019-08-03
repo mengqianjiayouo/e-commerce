3617fca934658072138726ca970ca10e461cfac6
mengqianjiayouo <mengqianjiayouo@sina.com>
2019-08-03 12:52:45 +0800

    Updates

---
+++ precache-manifest.e6784de60443dfcf8ff5ab77a5d5c525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14569e8e9fbb07a92fac4c2264a68a3",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "5dc4046b239cd9da20e2",
+    "url": "/e-commerce/static/css/2.34ae71ac.chunk.css"
+  },
+  {
+    "revision": "7672dbc473b48698178a",
+    "url": "/e-commerce/static/css/main.88b13e29.chunk.css"
+  },
+  {
+    "revision": "5dc4046b239cd9da20e2",
+    "url": "/e-commerce/static/js/2.fc21db52.chunk.js"
+  },
+  {
+    "revision": "7672dbc473b48698178a",
+    "url": "/e-commerce/static/js/main.145d3a23.chunk.js"
+  },
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  }
+]);
